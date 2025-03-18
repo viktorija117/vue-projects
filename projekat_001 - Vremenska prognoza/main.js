ba552db356633bb19c823153662e5abe976a1d87
viktorija117 <viktorija.kitanovic.17@gmail.com>
2025-03-17 22:23:26 -0700

    projekat_001 - Vremenska prognoza

---
+++ projekat_001 - Vremenska prognoza/main.js
@@ -0,0 +1,41 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            city: "",
+            weather: null,
+            error: "",
+            apiKey: "7c8eeb9c13a89a032328cb3959cf5c43"
+        };
+    },
+    computed()  {   
+        return {
+            weatherIcon() {
+                if (!this.weather) {
+                    return "";
+                }
+                return `http://openweathermap.org/img/wn/${this.weather.weather[0].icon}.png`;
+            }
+        }
+    },
+    methods: {
+        async fetchWeather() {
+            if (this.city === "") {
+                this.error = "Molimo unesite grad.";
+                return;
+            }
+            this.error = "";
+            this.weather = null;
+
+            try{
+                const response = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&lang=sr&appid=${this.apiKey}`
+                );
+                this.weather = response.data;
+            } catch (error) {
+                this.error = "Grad nije pronadjen.";
+            }
+        }
+    }
+});
+
+app.mount("#main");
